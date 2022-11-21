309298844a8a8538060aa2c50f55359673c6ac39
undefinedSynthesis <undefinedsynthesis@gmail.com>
2022-11-21 20:41:25 +0900

    パスを変更

--- api/main.py
+++ api/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/predicate")
-async def hello():
+@app.get("/")
+async def root():
     return {"message": "hello world!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000)
