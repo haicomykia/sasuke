71e0fbc29d20287a9c06b2baa6c32e72be5c9830
undefinedSynthesis <undefinedsynthesis@gmail.com>
2023-03-19 02:58:07 +0900

    logicパッケージの名前を修正 mypyを追加

--- api/main.py
+++ api/main.py
@@ -4,8 +4,8 @@ from schemas.schemas import Users
 from models import m_users
 from db import database
 from db.database import engine, get_db
-import logic.user as userLogic
-import logic.auth as authLogic
+import logic.user as user_logic
+import logic.auth as auth_logic
 
 database.ModelBase.metadata.create_all(bind=engine)
 
@@ -15,22 +15,22 @@ app = FastAPI()
 # Create
 @app.post('/user', response_model=Users)
 async def create_user(user: Users, db: Session = Depends(get_db)) -> m_users.Users:
-    return userLogic.create_user(db=db, user=user)
+    return user_logic.create_user(db=db, user=user)
 
 
 # Read
 @app.get('/user', response_model=list[Users])
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> m_users.Users:
-    users = userLogic.get_users(db=db, skip=skip, limit=limit)
+    users = user_logic.get_users(db=db, skip=skip, limit=limit)
     return users
 
 
 # User Auth
 @app.post('/user/auth')
 async def authorize_user(user: Users, db: Session = Depends(get_db)) -> bool:
-    return authLogic.verify_user(db=db, user=user)
+    return auth_logic.verify_user(db=db, user=user)
 
 # Leave User
 @app.post('/user/leave')
 async def leave_user(user: Users, db: Session = Depends(get_db)) -> bool:
-    return userLogic.leave_user(db=db, user=user)
+    return user_logic.leave_user(db=db, user=user)
