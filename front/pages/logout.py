133a556593dfa78b0ff45168f516dd3ea293f962
haicomykia <undefinedsynthesis@gmail.com>
2023-04-10 22:09:31 +0900

    ログアウトの機能を追加

--- front/pages/logout.py
+++ front/pages/logout.py
@@ -3,20 +3,21 @@ import streamlit as st
 import requests
 
 from core.settings import Settings
-from core.functions import  login_required
 
 settings = Settings()
 
-@login_required
 def main():
-    url = '{location}/auth/jwt/logout'.format(location=settings.AUTH_URL)
-    res = requests.post(url)
-    if res.status_code == status.HTTP_200_OK:
+    if 'access_token' in st.session_state:
+        url = '{location}/auth/jwt/logout'.format(location=settings.AUTH_URL)
+        res = requests.post(url)
+
         # セッションから削除
         for key in st.session_state.keys():
             del st.session_state[key]
+
+        st.info('ログアウトしました。')
     else:
-        st.error('ログアウトにて異常が発生しました。')
+        st.info('ログインしてください。')
 
 
 if __name__ == '__main__':
