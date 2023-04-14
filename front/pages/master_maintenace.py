708e36cc8702611a52f4d10fc3cd42af64c7716c
haicomykia <undefinedsynthesis@gmail.com>
2023-04-14 20:54:48 +0900

    first commit

---
+++ front/pages/master_maintenace.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+from core.settings import Settings
+from core.functions import login_required
+
+settings = Settings()
+
+@login_required
+def main():
+    st.markdown('# マスタメンテ画面')
+
+if __name__ == '__main__':
+    main()
