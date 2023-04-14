69a62123471bc4db16354af04005c1cc7937d092
haicomykia <undefinedsynthesis@gmail.com>
2023-04-14 21:08:15 +0900

    ログアウトの機能を修正

--- front/pages/logout.py
+++ front/pages/logout.py
@@ -7,17 +7,20 @@ from core.settings import Settings
 settings = Settings()
 
 def main():
-    if 'access_token' in st.session_state:
-        url = '{location}/auth/jwt/logout'.format(location=settings.AUTH_URL)
-        res = requests.post(url)
-
-        # セッションから削除
-        for key in st.session_state.keys():
-            del st.session_state[key]
-
-        st.info('ログアウトしました。')
-    else:
+    if 'access_token' not in st.session_state or (access_token := st.session_state['access_token']) is None:
         st.info('ログインしてください。')
+        return
+
+    url = '{location}/auth/jwt/logout'.format(location=settings.AUTH_URL)
+    headers = {'Authorization': f'Bearer {access_token}'}
+
+    res = requests.post(url, headers=headers)
+
+    # セッションから削除
+    for key in st.session_state.keys():
+        del st.session_state[key]
+
+    st.info('ログアウトしました。')
 
 
 if __name__ == '__main__':
